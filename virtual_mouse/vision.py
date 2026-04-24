37081a0231aae0bd895afbbdff239e69bd6b0eb4
Niya Shroff <102321655+niya-shroff@users.noreply.github.com>
2026-04-24 16:52:13 -0400

    vision class with reading the frame and ending for release

---
+++ virtual_mouse/vision.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+from config import config
+
+class Vision:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.hands = mp.solutions.hands.Hands(max_num_hands=1)
+        self.drawer = mp.solutions.drawing_utils
+
+    def read_frame(self):
+        success, frame = self.cap.read()
+        if not success:
+            return None, None
+
+        frame = cv2.flip(frame, 1)
+        h, w, _ = frame.shape
+
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.hands.process(rgb)
+
+        return frame, (result, w, h)
+
+    def release(self):
+        self.cap.release()
