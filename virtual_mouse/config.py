4c9b2d194dc96a1afc2cb000f3cbca5cb518b0d6
Niya Shroff <102321655+niya-shroff@users.noreply.github.com>
2026-04-24 16:49:41 -0400

    empty files and configs for clicking

---
+++ virtual_mouse/config.py
@@ -0,0 +1,13 @@
+class Config:
+    SMOOTHING = 5
+    FRAME_MARGIN = 100
+
+    ENABLE_CLICK = True
+    ENABLE_RIGHT_CLICK = True
+    ENABLE_DRAG = True
+    ENABLE_SCROLL = True
+
+    CLICK_DELAY = 0.5
+    RIGHT_CLICK_DELAY = 0.7
+
+config = Config()
